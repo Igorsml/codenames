ac462a07c75e82aa79fc6cc470172b69f9403857
Igorsml <igorsmlff@gmail.com>
2023-09-13 12:20:35 +0300

    feat: shuffle, styles

--- codenames/src/components/Cards/Cards.jsx
+++ codenames/src/components/Cards/Cards.jsx
@@ -6,6 +6,14 @@ import classes from "./Cards.module.scss";
 const limit = 25;
 wordsList.length = limit;
 
+function shuffleArray(array) {
+  array.forEach((_, i) => {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  });
+}
+shuffleArray(wordsList);
+
 export const Cards = () => {
   return (
     <ul className={classes.cardsList}>
